1396429e48a46424d44ffbdfc91fc9c0c8f81cf5
JuanManuelLopez <26lopez.jm@gmail.com>
2023-03-03 18:27:42 -0300

    es el nuevo

--- src/components/index/portada/ImagenPortada.jsx
+++ src/components/index/portada/ImagenPortada.jsx
@@ -7,14 +7,11 @@ const ImagenPortada = () => {
     const usarcont = useContext(CartContext)
     if(usarcont.scrorientation==='portrait'){
     return (
-        <div id='Portada_Video' className='col-12'>
-            <video id='portada_Video' autoPlay muted loop className='col-12' src='./assets/wallpapers/wallpaper-vertical.mp4'>
-            </video>
-        </div>
+            <img src="https://iili.io/HWnpSTb.jpg" alt="MT" className='col-12' />
         )
     }else{
         return(
-            <img src="https://iili.io/HW9yk3N.jpg" alt="MT" className='col-12'/>
+            <img src="https://iili.io/HWnD9dg.jpg" alt="MT" className='col-12'/>
         )
     }
 }
