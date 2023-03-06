b9fba3334cbcbf8a45c1eea70f3add38b584e565
JuanManuelLopez <26lopez.jm@gmail.com>
2023-03-06 09:52:24 -0300

    asdgfd

--- src/components/ItemDetail/TituloPortada.jsx
+++ src/components/ItemDetail/TituloPortada.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TituloPortada = (props) => {
   return (
-    <div className='position-absolute col-12 p-2 bottom-0 tituloportada'>
+    <div className='position-absolute col-12 p-2 tituloportada'>
         <h1 className='col-8'>{props.titulo}</h1>
     </div>
   )
